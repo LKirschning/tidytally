cb26aa3573da071dccbb48a0bb0ff9ea18720f2f
Christine Wolf <cw.wolf.christine@gmail.com>
2022-03-01 16:15:07 +0100

    rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_110743) do
+ActiveRecord::Schema.define(version: 2022_03_01_142248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_110743) do
     t.boolean "is_common"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["household_id"], name: "index_rooms_on_household_id"
   end
 
