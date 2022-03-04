ddf15e2c5e2176048dfbf2079d40ab34c0be8965
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-03-04 12:39:49 +0100

    implemented a bunch of stuff

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,16 @@
 class TasksController < ApplicationController
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to room_path(@room)
+    else
+      render :new
+    end
+  end
 end
