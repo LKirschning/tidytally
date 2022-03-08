65bc24d60bf2acc402e6fb5aca4ada1ef83abb41
Christine Wolf <cw.wolf.christine@gmail.com>
2022-03-08 16:11:27 +0100

    new rooms

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   get "/dashboard", to: "pages#dashboard"
   get "/select_household", to: "pages#select_household"
   resources :households, only: [:new, :create, :edit, :update]
-  resources :rooms, only: [:new, :create, :show, :index] do
+  resources :rooms, only: [:index,:show, :new, :create] do
     resources :tasks, only: [:new, :create]
   end
+  post "/add-rooms", to: "rooms#add_room", as: :add_room
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
