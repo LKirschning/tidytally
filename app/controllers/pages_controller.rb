d2623ed3373602f862457e344d147c10a1800366
Ludwig Kirschning <l.kirschning@yahoo.de>
2022-03-09 11:39:52 +0100

    fixed time on dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
+require "date"
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  helper_method :today_tomorrow
 
   def home
   end
@@ -18,17 +21,30 @@ class PagesController < ApplicationController
   def dashboard
     @household = current_user.households.last
     @housemates = @household.users
-    @tasks = []
-    @household.rooms.each do |room|
-      room.tasks.each do |task|
-        @tasks.push(task)
+    # @tasks = []
+    # @household.rooms.each do |room|
+    #   room.tasks.each do |task|
+    #     @tasks.push(task)
+    #   end
+    # end
+    # # the justice algortihm
+    # @tasks.each do |task|
+    #   task.user = @housemates.sample
+    # end
+    @user = current_user
+    def today_tomorrow(task_date)
+      date = Date.today
+      case task_date
+      when date.to_s
+        "today"
+      when (date + 1).to_s
+        "tomorrow"
+      else
+        task_date
       end
     end
-    # the justice algortihm
-    @tasks.each do |task|
-      task.user = @housemates.sample
-    end
-    @user = current_user
+    # @time = today_tomorrow(@task.time)
+
   end
 
   def select_household
